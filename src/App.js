36f231adc700922e7ce16c2dc9d3baff09c47bbd
Nilakshi2006 <Nilakshibhanot@gmail.com>
2026-06-20 16:41:44 +0530

    22(Setup+intro to REACT class based components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c="NILAKSHI"
+  render() {
+    return (
+      <div>
+        Hello!My name is {this.c}
+      </div>
+    )
+  }
+}
