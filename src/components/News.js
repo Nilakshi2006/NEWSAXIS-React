5d9fc4f540fea5d408c585c65b8e958bc0f16c5a
Nilakshi2006 <Nilakshibhanot@gmail.com>
2026-06-21 14:47:38 +0530

    24(Know about newsapi)

--- src/components/News.js
+++ src/components/News.js
@@ -7,9 +7,21 @@ export class News extends Component {
 
   render() {
     return (
-      <div>
-        NEWS COMPONENT
-        <NewsItem/>
+      <div className='container my-3'>
+
+        <h2>NEWSAXIS-Top Headlines</h2>
+        <div className="row">
+<div className="col-md-4">
+        <NewsItem title="Top Headline 1" description="This is the description for the first top headline." />
+        </div>
+        <div className="col-md-4">
+      <NewsItem title="Top Headline 2" description="This is the description for the second top headline." />
+      </div>
+      <div className="col-md-4">
+      <NewsItem title="Top Headline 3" description="This is the description for the third top headline." />
+      </div>
+     
+      </div>
       </div>
     )
   }
